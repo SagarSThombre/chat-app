77187681620d5272190c2819be58f3775b78874e
Sagar Thombare <st622644@gmail.com>
2026-02-01 02:02:45 +0530

    added repo

---
+++ src/main/java/com/chat_app_backend/repositories/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.chat_app_backend.repositories;
+
+import com.chat_app_backend.entities.Room;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RoomRepository extends MongoRepository<Room, String> {
+    // get room by roomId
+    Room findByRoomId(String roomId);
+}
