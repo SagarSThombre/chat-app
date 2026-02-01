ed49625a1a3185978c93e83c296a549506a95d74
Sagar Thombare <st622644@gmail.com>
2026-02-01 22:41:13 +0530

    webSocketConfig done

--- src/main/java/com/chat_app_backend/playload/MessageRequest.java
+++ src/main/java/com/chat_app_backend/playload/MessageRequest.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Setter
 @Getter
 @AllArgsConstructor
@@ -14,6 +16,4 @@ public class MessageRequest {
     private String content;
     private String sender;
     private String roomId;
-
-
 }
